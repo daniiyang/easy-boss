921d738c68f3941dac91467d5829f631fcca2d6e
daniiyang <lestreng.begi@gmail.com>
2020-10-23 20:20:49 +0300

    deploy

---
+++ build/precache-manifest.cab7f5bd65eb678ca956678dd7405eed.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53fe58089878d752ccd151cc6c4736da",
+    "url": "/easy-boss/build/index.html"
+  },
+  {
+    "revision": "959a764059fc64938167",
+    "url": "/easy-boss/build/static/css/2.1a0a2c47.chunk.css"
+  },
+  {
+    "revision": "30347bdcc8ed9807f23d",
+    "url": "/easy-boss/build/static/css/main.261a45fe.chunk.css"
+  },
+  {
+    "revision": "959a764059fc64938167",
+    "url": "/easy-boss/build/static/js/2.ede89a9c.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/easy-boss/build/static/js/2.ede89a9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30347bdcc8ed9807f23d",
+    "url": "/easy-boss/build/static/js/main.4ee43dea.chunk.js"
+  },
+  {
+    "revision": "3de084524cd8fb03a513",
+    "url": "/easy-boss/build/static/js/runtime-main.89194455.js"
+  },
+  {
+    "revision": "0d2a0e1d516626463a882abd2341c85e",
+    "url": "/easy-boss/build/static/media/circleWave1.0d2a0e1d.PNG"
+  },
+  {
+    "revision": "861a48c46a13b165c2160cd4b3775ae5",
+    "url": "/easy-boss/build/static/media/circleWave2.861a48c4.PNG"
+  },
+  {
+    "revision": "825516725049e30bb3d9e1030951a965",
+    "url": "/easy-boss/build/static/media/imgPost1.82551672.PNG"
+  },
+  {
+    "revision": "31c66c221c97f39ad41ea6dac514d033",
+    "url": "/easy-boss/build/static/media/imgPost2.31c66c22.PNG"
+  },
+  {
+    "revision": "832a0ad89f0e7c8abde3fb19bc65233a",
+    "url": "/easy-boss/build/static/media/imgPost3.832a0ad8.PNG"
+  }
+]);
